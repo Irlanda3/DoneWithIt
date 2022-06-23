4b673c13abad226d726e9d68831e2a2c7805f342
aochoa1@msudenver.edu <aocho1@msudenver.edu>
2022-06-23 16:48:05 -0600

    adding image

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   console.log("App executed");
-
+  //const handlePress = () => console.log("Text pressed");
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text> Hello React Native</Text>
+      <Image 
+        blurRadius={10}
+        fadeDuration={1000}
+        source={{
+        
+        width: 200,
+        height: 300,
+        uri: "https://picsum.photos/200/300"}} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     alignItems: 'center',
     justifyContent: 'center',
   },
