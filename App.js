939ee508c21bd05853bc596c535b7f39eacc13ab
aochoa1@msudenver.edu <aocho1@msudenver.edu>
2022-06-27 16:12:15 -0600

    issue with flex

--- App.js
+++ App.js
@@ -1,24 +1,15 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View, Image, SafeAreaView, Button } from 'react-native';
+import { styleSheet, View } from 'react-native';
+
 
 export default function App() {
-  console.log("App executed");
-  //const handlePress = () => console.log("Text pressed");
-
+ 
   return (
-    <SafeAreaView style={styles.container}>
-      <Button 
-        color="orange"
-        title ="Click Me" onPress={() => console.log("Button tap")}/>
-    </SafeAreaView>
+    <View style={{
+      backgroundColor: 'green',
+      flex: 1,
+    }}></View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
